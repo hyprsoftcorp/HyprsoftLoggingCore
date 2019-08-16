c74674b4cf209ae40eda478ac6937e17b5e16407
Hal Blakeslee <hal@hyprsoft.com>
2019-08-15 22:01:12 -0700

    Added a static logger that logs exceptions to individual json files.

---
+++ Hyprsoft.Logging.Core/ExceptionLogger.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace Hyprsoft.Logging.Core
+{
+    public class ExceptionLogger
+    {
+        #region Fields
+
+        private readonly object _lock = new object();
+        private static ExceptionLogger _instance;
+
+        #endregion
+
+        #region Constructors
+
+        private ExceptionLogger() { }
+
+        #endregion
+
+        #region Properties
+
+        public const string DefaultLogFilenameExtension = "json";
+        public const string DefaultLogFilenameDateFormat = "yyyy-MM-dd-hh-mm-ss-fff";
+
+        public static ExceptionLogger Instance { get { return _instance ?? (_instance = new ExceptionLogger()); } }
+
+        public string RootFolder { get; set; } = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+        public string LogFilenameExtension { get; set; } = DefaultLogFilenameExtension;
+
+        public string LogFilenameDateFormat { get; set; } = DefaultLogFilenameDateFormat;
+
+        #endregion
+
+        #region Methods
+
+        public string Log(Exception ex)
+        {
+            lock (_lock)
+            {
+                var logFilename = Path.Combine(RootFolder, $"{DateTime.Now.ToString(LogFilenameDateFormat)}-{ex.GetType().Name}.{LogFilenameExtension}").ToLower();
+                using (var writer = File.AppendText(logFilename))
+                    writer.Write(JsonConvert.SerializeObject(ex, Formatting.Indented));
+                return logFilename;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Hyprsoft.Logging.Tests/ExceptionLoggerTests.cs
@@ -0,0 +1,28 @@
+﻿using Hyprsoft.Logging.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace Hyprsoft.Logging.Tests
+{
+    [TestClass]
+    public class ExceptionLoggerTests
+    {
+        [TestMethod]
+        public void Success()
+        {
+            string logFilename;
+            try
+            {
+                throw new InvalidOperationException("Oops");
+            }
+            catch (Exception ex)
+            {
+                logFilename = ExceptionLogger.Instance.Log(ex);
+            }
+
+            Assert.IsTrue(File.Exists(logFilename));
+            File.Delete(logFilename);
+        }
+    }
+}
